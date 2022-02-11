d70887032ad72063648eb15e45ebec567e21c73f
Stephan Karl <steph@Stephans-MacBook-Pro.local>
2022-02-11 16:05:26 +0000

    logo removed

--- src/App.js
+++ src/App.js
@@ -6,21 +6,7 @@ import { Button } from '@mui/material'
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Small Change
-        </a>
-        <Button variant='contained'>This is a button</Button>
-      </header>
+      <Button variant='contained'>This is a button</Button>
     </div>
   );
 }
